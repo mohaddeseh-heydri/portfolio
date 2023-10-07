c542411126b128b0419396b156a66b245ffcb871
moh 13781201 <mhydry121@gmail.com>
2023-10-07 12:56:02 +0330

    portfolio

--- src/components/Projectlist.jsx
+++ src/components/Projectlist.jsx
@@ -1,9 +1,8 @@
 import ProjectCard from "../components/Projectcard";
 import { Container, Row, Col } from "react-bootstrap";
-import eduwell from '/src/assets/images/eduwell.png'
+import eduwell from "/src/assets/images/eduwell.png";
 import chat from "/src/assets/images/chat.png";
 import "../pages/style.css";
-
 
 function Projectlist() {
   return (
@@ -27,6 +26,19 @@ function Projectlist() {
                 isBlog={false}
                 title="chat"
                 description="Sample project made react and tailwind "
+                ghLink="https://github.com/mohaddeseh-heydri/FarawinLoginReact1/tree/main/mohaddesehHeydari"
+              />
+            </Col>
+            <Col md={4} className="project-card">
+              <ProjectCard
+                imgPath={chat}
+                isBlog={false}
+                title="chat"
+                description="Multiple Pages (React Router): The website is built with React Router v6 to enable easy access to multiple pages, showcasing various aspects of my skills and projects.
+
+                Fully Responsive: The website is fully responsive and works flawlessly on all devices, ensuring a seamless user experience across different screen sizes.
+                
+                Modern Design: The portfolio website features a modern and visually appealing design, utilizing animations to add an interactive touch to the user interface."
                 ghLink="https://github.com/mohaddeseh-heydri/FarawinLoginReact1/tree/main/mohaddesehHeydari"
               />
             </Col>
